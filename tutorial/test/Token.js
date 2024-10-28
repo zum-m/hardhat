5195a080fa3ce97dd2e92882c44d538102a9be02
zum-m <89333894+zum-m@users.noreply.github.com>
2024-10-29 01:46:22 +0900

    [blog]ファクトリーとフィクスチャーの違い。

--- tutorial/test/Token.js
+++ tutorial/test/Token.js
@@ -1,69 +1,68 @@
-const { expect } = require('chai'); // Chaiのexpect関数をインポート
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers"); // Hardhat Toolboxのフィクスチャーを利用
 
-describe('Tokenコントラクト', () => { // Tokenコントラクトのテストスイートを定義
-  let Token; // コントラクトのファクトリを格納する変数
-  let hardhatToken; // デプロイされたコントラクトのインスタンスを格納する変数
-  let owner; // コントラクトのオーナーアカウント
-  let addr1; // テストに使用するアカウント1
-  let addr2; // テストに使用するアカウント2
-  let addrs; // その他のテストに使用するアカウント
 
-  beforeEach(async () => { // 各テストの前に実行されるセットアップコード
-    Token = await ethers.getContractFactory('Token'); // Tokenコントラクトのファクトリを取得
-    [owner, addr1, addr2, ...addrs] = await ethers.getSigners(); // テストに使用するアカウントを取得
+describe('Tokenコントラクト', () => {
+  let Token;
+  let hardhatToken;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
 
-    hardhatToken = await Token.deploy(); // Tokenコントラクトをデプロイ
+  beforeEach(async () => {
+    Token = await ethers.getContractFactory('Token');
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    hardhatToken = await Token.deploy();
   });
 
-  describe('デプロイ', () => { // デプロイに関するテスト
-    it('正しいオーナーが設定されているべき', async () => { // コントラクトのオーナーが正しく設定されているかを確認
-      expect(await hardhatToken.owner()).to.equal(owner.address); // オーナーアドレスが正しいかを確認
+  describe('デプロイ', () => {
+    it('正しいオーナーが設定されているべき', async () => {
+      expect(await hardhatToken.owner()).to.equal(owner.address);
     });
 
-    it('トークンの総供給量がオーナーに割り当てられるべき', async () => { // トークンの総供給量がオーナーに割り当てられているかを確認
-      const ownerBalance = await hardhatToken.balanceOf(owner.address); // オーナーのトークン残高を取得
-      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance); // 総供給量がオーナーの残高と一致するかを確認
+    it('トークンの総供給量がオーナーに割り当てられるべき', async () => {
+      const ownerBalance = await hardhatToken.balanceOf(owner.address);
+      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
     });
   });
 
-  describe('トランザクション', () => { // トランザクションに関するテスト
-    it('アカウント間でトークンが転送されるべき', async () => { // アカウント間でトークンが正しく転送されるかを確認
-      await hardhatToken.transfer(addr1.address, 50); // addr1に50トークンを転送
-      const addr1Balance = await hardhatToken.balanceOf(addr1.address); // addr1のトークン残高を取得
-      expect(addr1Balance).to.equal(50); // addr1の残高が50であることを確認
+  describe('トランザクション', () => {
+    it('アカウント間でトークンが転送されるべき', async () => {
+      await hardhatToken.transfer(addr1.address, BigInt(50));
+      const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(BigInt(50));
 
-      await hardhatToken.connect(addr1).transfer(addr2.address, 50); // addr1からaddr2に50トークンを転送
-      const addr2Balance = await hardhatToken.balanceOf(addr2.address); // addr2のトークン残高を取得
-      expect(addr2Balance).to.equal(50); // addr2の残高が50であることを確認
+      await hardhatToken.connect(addr1).transfer(addr2.address, BigInt(50));
+      const addr2Balance = await hardhatToken.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(BigInt(50));
     });
 
-    it('送信者が十分なトークンを持っていない場合、失敗するべき', async () => { // トークンが不足している場合に転送が失敗するかを確認
-      const initialOwnerBalance = await hardhatToken.balanceOf(owner.address); // オーナーの初期トークン残高を取得
+    it('送信者が十分なトークンを持っていない場合、失敗するべき', async () => {
+      const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
 
       await expect(
-        hardhatToken.connect(addr1).transfer(owner.address, 1) // addr1がオーナーに1トークンを転送しようとする
-      ).to.be.revertedWith('Not enough tokens'); // トークン不足でエラーが発生することを確認
+        hardhatToken.connect(addr1).transfer(owner.address, BigInt(1))
+      ).to.be.revertedWith('Not enough tokens');
 
-      expect(await hardhatToken.balanceOf(owner.address)).to.equal(
-        initialOwnerBalance // オーナーの残高が変わっていないことを確認
-      );
+      expect(await hardhatToken.balanceOf(owner.address)).to.equal(initialOwnerBalance);
     });
 
-    it('転送後に残高が更新されるべき', async () => { // 転送後に残高が正しく更新されるかを確認
-      const initialOwnerBalance = await hardhatToken.balanceOf(owner.address); // オーナーの初期トークン残高を取得
+    it('転送後に残高が更新されるべき', async () => {
+      const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
 
-      await hardhatToken.transfer(addr1.address, 100); // addr1に100トークンを転送
-      await hardhatToken.transfer(addr2.address, 50); // addr2に50トークンを転送
+      await hardhatToken.transfer(addr1.address, BigInt(100));
+      await hardhatToken.transfer(addr2.address, BigInt(50));
 
-      const finalOwnerBalance = await hardhatToken.balanceOf(owner.address); // オーナーの最終トークン残高を取得
-      expect(finalOwnerBalance).to.equal(initialOwnerBalance - 150); // オーナーの残高が正しく減少していることを確認
+      const finalOwnerBalance = await hardhatToken.balanceOf(owner.address);
+      expect(finalOwnerBalance).to.equal(initialOwnerBalance - BigInt(150));
 
-      const addr1Balance = await hardhatToken.balanceOf(addr1.address); // addr1のトークン残高を取得
-      expect(addr1Balance).to.equal(100); // addr1の残高が100であることを確認
+      const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(BigInt(100));
 
-      const addr2Balance = await hardhatToken.balanceOf(addr2.address); // addr2のトークン残高を取得
-      expect(addr2Balance).to.equal(50); // addr2の残高が50であることを確認
+      const addr2Balance = await hardhatToken.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(BigInt(50));
     });
   });
 
