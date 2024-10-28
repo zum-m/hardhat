ed4cc22b843096e31ef7bc447705f55f93207c60
zum-m <89333894+zum-m@users.noreply.github.com>
2024-10-29 01:57:54 +0900

    [edit]Hardhar Networkの設定変更

--- tutorial/hardhat.config.js
+++ tutorial/hardhat.config.js
@@ -3,5 +3,15 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      mining: {
+        auto: false,
+        interval: [10000, 11000]
+      }
+    },
+  },
 };
+
 
