782094a4e9bb2b1fdd6a772729000256eadf1238
zum-m <89333894+zum-m@users.noreply.github.com>
2024-10-29 03:45:19 +0900

    [fix] deployを動かすために、hre（Hardhat Runtime Environment）,hardhatモジュールインポートを行った。

--- tutorial/scripts/deploy.js
+++ tutorial/scripts/deploy.js
@@ -1,15 +1,26 @@
+const hre = require("hardhat");
+
 async function main() {
-    const [deployer] = await ethers.getSigners();
+    const [deployer] = await hre.ethers.getSigners();
 
-    console.log("Deploying contracts with the account:", deployer.address);
+    console.log("アカウントでコントラクトをデプロイしています:", deployer.address);
 
-    const balance = await deployer.getBalance();
-    console.log("Account balance:", balance.toString());
+    const balance = await hre.ethers.provider.getBalance(deployer.address);
+    console.log("アカウントの残高:", balance.toString());
 
-    const Contract = await ethers.getContractFactory("YourContractName");
+    const Contract = await hre.ethers.getContractFactory("Token");
     const contract = await Contract.deploy();
 
-    console.log("Contract deployed to address:", contract.address);
+    // デプロイメントの完了を待つ
+    const receipt = await contract.deployTransaction.wait();
+
+    console.log("コントラクトがデプロイされたアドレス:", contract.address);
+
+    // トランザクションIDの確認
+    console.log("トランザクションID:", contract.deployTransaction.hash);
+
+    // デプロイのために消費したGasの確認
+    console.log("消費されたGas:", receipt.gasUsed.toString());
 }
 
 main()
